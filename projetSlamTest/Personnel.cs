d3c1117c5ca157da2ac41084ffd33ba43f2d8508
FlouKiller <40624699+FlouKiller@users.noreply.github.com>
2023-10-03 11:54:05 +0200

    ratio adrien

---
+++ projetSlamTest/Personnel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetSlamTest
+{
+    internal class Personnel
+    {
+        private string matricule;
+        private DateTime dateEmbauche;
+        private string motDePasse;
+        private int type;
+    }
+}
