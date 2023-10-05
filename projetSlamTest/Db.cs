74c44af41abb966da5f6056b0bcff01df026a94f
FlouKiller <40624699+FlouKiller@users.noreply.github.com>
2023-10-05 10:24:08 +0200

    bd

--- projetSlamTest/Db.cs
+++ projetSlamTest/Db.cs
@@ -1,7 +1,18 @@
-﻿namespace projetSlamTest
+﻿using MySql.Data.MySqlClient;
+
+namespace projetSlamTest
 {
-    public class Db
+    public static class Db
     {
         // classe de la base de donnée
+
+        private string connectionString;
+        private MySqlConnection connection;
+
+        public Db(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
     }
 }
