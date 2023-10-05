a25c60236215af8c496178f3219e4178d04ab01d
FlouKiller <40624699+FlouKiller@users.noreply.github.com>
2023-10-05 09:41:13 +0200

    mise a jour classes

--- projetSlamTest/Technicien.cs
+++ projetSlamTest/Technicien.cs
@@ -6,28 +6,22 @@ using System.Threading.Tasks;
 
 namespace projetSlamTest
 {
-    public class Technicien
+    public class Technicien : Personnel
     {
-
-
-        public Technicien(int id, string niveau, string formation, string competences, string matricule)
+        public Technicien(string matricule, DateTime dateEmbauche, string motDePasse, int type, int materielId, int id, string niveau, string formation, string competences) : base(matricule, dateEmbauche, motDePasse, type, materielId)
         {
+
             Id = id;
             Niveau = niveau;
             Formation = formation;
             Competences = competences;
-            Matricule = matricule;
+
         }
 
         public int Id { get; set; }
 
         public string Niveau { get; set; }
-
         public string Formation { get; set; }
-
         public string Competences { get; set; }
-
-        public string Matricule { get; set; }
-
     }
 }
