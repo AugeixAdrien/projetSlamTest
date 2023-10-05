727edb57c4109fcd54b4885f6cb40f73b005fdbb
AUGEIX Adrien <58886109+AugeixAdrien@users.noreply.github.com>
2023-10-05 09:23:46 +0200

    creation des classes

--- projetSlamTest/Materiel.cs
+++ projetSlamTest/Materiel.cs
@@ -1,7 +1,37 @@
-﻿namespace projetSlamTest
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace projetSlamTest
 {
     public class Materiel
     {
-        // adrien
+        public Materiel (int id, string processeur, string memoire, string disque, List<string> logiciels, DateTime dateAchat, string garantie, string fournisseur)
+        {
+            Id = id;
+            Processeur = processeur;
+            Memoire = memoire;
+            Disque = disque;
+            Logiciels = logiciels;
+            DateAchat = dateAchat;
+            Garantie = garantie;
+            Fournisseur = fournisseur;
+        }
+
+        public int Id { get; set; }
+
+        public string Fournisseur { get; set; }
+
+        public string Garantie { get; set; }
+
+        public DateTime DateAchat { get; set; }
+
+        public List<string> Logiciels { get; }
+
+        public string Disque { get; set; }
+
+        public string Memoire { get; set; }
+
+        public string Processeur { get; set; }
     }
 }
--- projetSlamTest/Ticket.cs
+++ projetSlamTest/Ticket.cs
@@ -1,7 +1,43 @@
-﻿namespace projetSlamTest
+﻿using System;
+
+namespace projetSlamTest
 {
     public class Ticket
     {
-        // adrien
+        // un ticket a :
+        // un id : int
+        // un objet : string
+        // un niveau d'urgence : int
+        // une date de creation : DateTime
+        // un etat : string
+        // un idTechnicien : int
+        // un idMateriel : int
+        
+        // fais le contructeur de la classe
+        
+        public Ticket (int id, string objet, int niveauUrgence, DateTime dateCreation, string etat, int idTechnicien, int idMateriel)
+        {
+            Id = id;
+            Objet = objet;
+            NiveauUrgence = niveauUrgence;
+            DateCreation = dateCreation;
+            Etat = etat;
+            IdTechnicien = idTechnicien;
+            IdMateriel = idMateriel;
+        }
+
+        public int IdMateriel { get; set; }
+
+        public int IdTechnicien { get; set; }
+
+        public string Etat { get; set; }
+
+        public DateTime DateCreation { get; set; }
+
+        public int NiveauUrgence { get; set; }
+
+        public string Objet { get; set; }
+
+        public int Id { get; set; }
     }
 }
